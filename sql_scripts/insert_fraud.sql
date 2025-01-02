89236d2222ee7f9f7f390faac11f4647eb1bbe7f
Kseniia Karnakova <144525678+xenahkar@users.noreply.github.com>
2025-01-02 17:43:31 +0300

    updated type 4

--- sql_scripts/insert_fraud.sql
+++ sql_scripts/insert_fraud.sql
@@ -8,27 +8,27 @@ INSERT INTO
         event_type,
         report_dt
     )
-SELECT DISTINCT
+SELECT
     kdft.trans_date AS event_dt,
     kddc2.passport_num AS passport_num,
     CONCAT(kddc2.last_name, ' ', kddc2.first_name, ' ', kddc2.patronymic) AS fio,
     kddc2.phone AS phone,
     1 AS event_type,
     TO_DATE(%s, 'YYYY-MM-DD') AS report_dt
-FROM 
+FROM
     public.kkar_dwh_fact_transactions kdft
-LEFT JOIN 
+LEFT JOIN
     public.kkar_dwh_dim_cards kddc ON TRIM(kdft.card_num) = TRIM(kddc.card_num)
-LEFT JOIN 
+LEFT JOIN
     public.kkar_dwh_dim_accounts kdda ON kddc.account_num = kdda.account_num
-LEFT JOIN 
+LEFT JOIN
     public.kkar_dwh_dim_clients kddc2 ON kdda.client = kddc2.client_id
 WHERE
-    	-- заблокированный паспорт
-    	kddc2.passport_num IN (SELECT passport_num FROM public.kkar_dwh_fact_passport_blacklist)
-    	-- просроченный паспорт
-	    OR kddc2.passport_valid_to < DATE(kdft.trans_date)
-	    AND NOT EXISTS (
+    (
+        kddc2.passport_num IN (SELECT passport_num FROM public.kkar_dwh_fact_passport_blacklist)
+        OR kddc2.passport_valid_to < DATE(kdft.trans_date)
+    )
+    AND NOT EXISTS (
         SELECT 1
         FROM public.kkar_rep_fraud t
         WHERE
@@ -40,6 +40,7 @@ WHERE
     )
 GROUP BY
     kdft.trans_date, kddc2.passport_num, fio, kddc2.phone;
+
 
 -- type 2. Совершение операции при недействующем договоре
 INSERT into public.kkar_rep_fraud(
@@ -134,4 +135,78 @@ WHERE
             AND t3.fio = t1.fio
             AND t3.phone = t1.phone
             AND t3.event_type = 3
+    );
+-- type 4. Попытка подбора суммы
+-- В течение 20 минут происходит более 3 операций с последовательными суммами,
+-- где каждая последующая меньше предыдущей, при этом отклонены все, кроме последней.
+-- Последняя операция (успешная) в такой цепочке считается мошеннической.
+WITH temp1 AS (
+    SELECT
+        client_id,
+        trans_date AS event_dt,
+        row_number() OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS rn,
+        EXTRACT(EPOCH FROM (trans_date - LAG(trans_date, 3) OVER (PARTITION BY client_id ORDER BY trans_date ASC))) AS time_diff,
+        LAG(amt) OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS prev_amt,
+        LAG(amt, 2) OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS prev_prev_amt,
+        LAG(amt, 3) OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS prev_prev_prev_amt,
+        LAG(oper_result) OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS prev_status,
+        LAG(oper_result, 2) OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS prev_prev_status,
+        LAG(oper_result, 3) OVER (PARTITION BY client_id ORDER BY trans_date ASC) AS prev_prev_prev_status,
+        amt,
+        oper_result,
+        passport_num,
+        CONCAT(last_name, ' ', first_name, ' ', patronymic) AS fio,
+        phone,
+        4 AS event_type
+    FROM
+        public.kkar_dwh_fact_transactions kdft
+        LEFT JOIN public.kkar_dwh_dim_cards kgdc ON TRIM(kdft.card_num) = TRIM(kgdc.card_num)
+        LEFT JOIN public.kkar_dwh_dim_accounts kdda ON kgdc.account_num = kdda.account_num
+        LEFT JOIN public.kkar_dwh_dim_clients kddc2 ON kdda.client = kddc2.client_id
+        LEFT JOIN public.kkar_dwh_dim_terminals kddt ON kdft.terminal = kddt.terminal_id
+),
+temp2 AS (
+    SELECT
+        *
+    FROM temp1
+    WHERE
+        -- в условии "более 3 операций", поэтому я искала три отклоненные и последнюю успешную
+        oper_result = 'SUCCESS'
+        AND prev_status = 'REJECT'
+        AND prev_prev_status = 'REJECT'
+        AND prev_prev_prev_status = 'REJECT'
+         -- в течение 20 минут
+        AND time_diff <= 1200
+        --  каждая последующая операция меньше предыдущей
+        AND prev_prev_prev_amt > prev_prev_amt
+        AND prev_prev_amt > prev_amt
+        AND prev_amt > amt
+)
+INSERT INTO
+	public.kkar_rep_fraud(
+		event_dt,
+		passport_num,
+		fio,
+		phone,
+		event_type,
+		report_dt
+	)
+SELECT
+    event_dt,
+    passport_num,
+    fio,
+    phone,
+    event_type,
+    TO_DATE(%s, 'YYYY-MM-DD') AS report_dt
+FROM
+    temp2
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM public.kkar_rep_fraud t
+        WHERE
+            t.event_dt = temp2.event_dt
+            AND t.passport_num = temp2.passport_num
+            AND t.fio = temp2.fio
+            AND t.phone = temp2.phone
+            AND t.event_type = 4
     );
